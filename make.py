2b6b91f11da5dd89a383854265116be9d3efdb89
Maximilian Hils <git@maximilianhils.com>
2022-10-06 17:05:31 +0200

    setup++

--- make.py
+++ make.py
@@ -10,22 +10,25 @@ from markupsafe import Markup
 
 here = Path(__file__).parent
 
-job = here / "resources" / "job.yml"
 
 if __name__ == "__main__":
-    env = Environment(loader=FileSystemLoader([here]))
-
     lexer = pygments.lexers.YamlLexer()
     formatter = pygments.formatters.html.HtmlFormatter(style="friendly")
     pygments_css = formatter.get_style_defs()
-    job_html = Markup(pygments.highlight(job.read_text("utf8"), lexer, formatter))
+
+    def example(filename: str) -> Markup:
+        contents = (here / filename).read_text("utf8")
+        return Markup(pygments.highlight(contents, lexer, formatter)).replace(
+            "@hash",
+            Markup('@<span class="action-hash">ffd35390d6449f911cccb7694edeeecd7c4d8d87</span>')
+        )
+
+    env = Environment(loader=FileSystemLoader([here]))
+    env.globals["example"] = example
 
     for page in ["index", "privacy", "security", "setup"]:
         (here / f"{page}.html").write_bytes(
             env.get_template(f"{page}.html.jinja2")
-            .render(
-                job_html=job_html,
-                pygments_css=pygments_css,
-            )
+            .render(pygments_css=pygments_css,)
             .encode()
         )
