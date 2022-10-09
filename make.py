9432dd934eba1ce6bfa445c68a1fde40b11b2b6c
Maximilian Hils <git@maximilianhils.com>
2022-10-09 17:03:28 +0200

    use @install-pinned, improve copywriting

--- make.py
+++ make.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import shutil
-import textwrap
+import re
 from pathlib import Path
 
 import pygments.formatters.html
@@ -10,18 +9,23 @@ from markupsafe import Markup
 
 here = Path(__file__).parent
 
-
 if __name__ == "__main__":
     lexer = pygments.lexers.YamlLexer()
     formatter = pygments.formatters.html.HtmlFormatter(style="friendly")
     pygments_css = formatter.get_style_defs()
 
+
     def example(filename: str) -> Markup:
         contents = (here / filename).read_text("utf8")
-        return Markup(pygments.highlight(contents, lexer, formatter)).replace(
-            "@hash",
-            Markup('@<span class="action-hash">ffd35390d6449f911cccb7694edeeecd7c4d8d87</span>')
-        )
+        code = pygments.highlight(contents, lexer, formatter)
+
+        def hash_sub(m: re.Match) -> str:
+            default = m[2] or "ffd35390d6449f911cccb7694edeeecd7c4d8d87"
+            return f'{m[1]}@<span class="action-hash" data-repo="{m[1]}">{default}</span>'
+
+        code = re.sub(r"([0-9a-zA-Z._/-]+)@hash(?::([0-9a-f]+))?", hash_sub, code)
+        return Markup(code)
+
 
     env = Environment(loader=FileSystemLoader([here]))
     env.globals["example"] = example
@@ -29,6 +33,6 @@ if __name__ == "__main__":
     for page in ["index", "privacy", "security", "setup"]:
         (here / f"{page}.html").write_bytes(
             env.get_template(f"{page}.html.jinja2")
-            .render(pygments_css=pygments_css,)
+            .render(pygments_css=pygments_css, )
             .encode()
         )
