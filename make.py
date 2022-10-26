127e5121d9efcc3d8a9bb333faa686a8fcff7c1c
Maximilian Hils <git@maximilianhils.com>
2022-10-26 22:08:29 +0200

    use dependabot for updates

--- make.py
+++ make.py
@@ -2,9 +2,8 @@
 import re
 from pathlib import Path
 
-import httpx_cache
 import pygments.formatters.html
-import pygments.lexers.python
+import pygments.lexers
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from markupsafe import Markup
@@ -17,20 +16,13 @@ if __name__ == "__main__":
     pygments_css = formatter.get_style_defs()
 
     def example(filename: str) -> Markup:
-        contents = (here / filename).read_text("utf8")
+        contents = (here / ".github" / "workflows" / filename).read_text("utf8")
         code = pygments.highlight(contents, lexer, formatter)
 
         def hash_sub(m: re.Match) -> str:
-            with httpx_cache.Client(cache=httpx_cache.FileCache()) as client:
-                default = client.get(
-                    f"https://api.github.com/repos/{m[1]}/branches/main",
-                    headers={"cache-control": "max-age=86400"},
-                ).json()["commit"]["sha"]
-            return (
-                f'{m[1]}@<span class="action-hash" data-repo="{m[1]}">{default}</span>'
-            )
+            return f'{m[1]}@<span class="action-hash">{m[2]}</span>'
 
-        code = re.sub(r"([0-9a-zA-Z._/-]+)@hash(?::([0-9a-f]+))?", hash_sub, code)
+        code = re.sub(r"([0-9a-zA-Z._/-]+)@([0-9a-f]{40})", hash_sub, code)
         return Markup(code)
 
     env = Environment(loader=FileSystemLoader([here]))
